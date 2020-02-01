bb6e97af89283af96aca254e2ddc4d851758dbaa
Dawid Kraczkowski <dawid.kraczkowski@gmail.com>
2020-02-01 20:09:38 +0100

    Changed port to environ

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-from chocs import serve, router
+from chocs import serve
 from kink import di
+from os import environ
 
 from chinook import create_database
 from chinook.middleware import check_json_request
@@ -7,4 +8,4 @@ from chinook.api import *
 
 if __name__ == "__main__":
     create_database(di["database_path"], di["database_schema"])
-    serve(check_json_request, router, host="0.0.0.0", port=5000)
+    serve(check_json_request, router, host="0.0.0.0", port=environ.get("PORT"))
