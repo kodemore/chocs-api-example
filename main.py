9c849114d5db8fef5a39b3a7613929ac3d3878cc
Dawid Kraczkowski <dawid.kraczkowski@gmail.com>
2020-02-01 20:12:08 +0100

    Changed port to int value

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from chocs import serve
 from kink import di
-from os import environ
+import os
 
 from chinook import create_database
 from chinook.middleware import check_json_request
@@ -8,4 +8,4 @@ from chinook.api import *
 
 if __name__ == "__main__":
     create_database(di["database_path"], di["database_schema"])
-    serve(check_json_request, router, host="0.0.0.0", port=environ.get("PORT"))
+    serve(check_json_request, router, host="0.0.0.0", port=int(os.environ.get('PORT', 5000)))
